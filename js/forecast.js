9a4e47c0cfde6fb0471ce926b1bcf54d20e94f4e
Yang Wang <wang195@usc.edu>
2015-11-10 15:12:02 -0800

    version 4

--- js/forecast.js
+++ js/forecast.js
@@ -32,7 +32,7 @@ function GetMap(latitude, longitude) {
 		{
 			isBaseLayer: false,
 			opacity: 0.6,
-			sphericalMercator: ture
+			sphericalMercator: true
 		}
 	);
 	var precipitation = new OpenLayers.Layer.XYZ(
@@ -48,12 +48,26 @@ function GetMap(latitude, longitude) {
 	map.setCenter(position, 10);
 }
 
+function time_transfer(ori_time){
+	var str = '';
+	var time = new Date(ori_time),
+	h = time.getHours(), // 0-24 format
+	m = time.getMinutes();
+	
+	if (h > 12){
+		str = ('00' + (h - 12)).substr(-2) + ':' + ('00' + m).substr(-2) + ' PM';
+	} else if (h == 0){
+		str = '12:' + ('00' + m).substr(-2) + ' AM';
+	} else{
+		str = ('00' + h).substr(-2) + ':' + ('00' + m).substr(-2) + ' AM';
+	}
 
-function data_deploy(data, city, state, celsius){
-	//Part 1: display current weather 
-	var cur = data.currently;
+	return str;
+}
+
+function icon_transfer(ori_icon){
 	var icon_url = '';
-	switch (cur.icon){
+	switch (ori_icon){
 		case 'clear-day': icon_url = 'http://cs-server.usc.edu:45678/hw/hw8/images/clear.png'; break;
 		case 'clear-night': icon_url = 'http://cs-server.usc.edu:45678/hw/hw8/images/clear_night.png'; break;
 		case 'rain': icon_url = 'http://cs-server.usc.edu:45678/hw/hw8/images/rain.png'; break;
@@ -65,15 +79,17 @@ function data_deploy(data, city, state, celsius){
 		case 'partly-cloudy-day': icon_url = 'http://cs-server.usc.edu:45678/hw/hw8/images/cloud_day.png'; break;
 		case 'partly-cloudy-night': icon_url = 'http://cs-server.usc.edu:45678/hw/hw8/images/cloud_night.png'; break;
 	}
+	return icon_url;
+}
 
-	$('#current-weather-sum #current-weather-icon').attr('src', icon_url);
+function data_deploy(data, city, state, celsius){
+	/* Part 1: display current weather */ 
+	var cur = data.currently;
+
+	$('#current-weather-sum #current-weather-icon').attr('src', icon_transfer(cur.icon));
 	$('#current-weather-sum #current-weather-desciption').text('Mostly '+cur.summary+' in '+city+', '+state);
-	if (celsius){
-		$('#current-weather-sum #current-weather-temp').text(parseInt(cur.temperature)+'<sup>o</sup>C');
-	} else {
-		$('#current-weather-sum #current-weather-temp').text(parseInt(cur.temperature)+'<sup>o</sup>F');
-	}
-	$('#current-weather-sum #current-weather-lhtemp').text(parseInt(data.daily.data[0].temperatureMax)+' | '+parseInt(data.daily.data[0].temperatureMin));
+	$('#current-weather-sum #current-weather-temp').text(parseInt(cur.temperature));
+	$('#current-weather-sum #current-weather-lhtemp').text(parseInt(data.daily.data[0].temperatureMax)+'° | '+parseInt(data.daily.data[0].temperatureMin)+'°');
 
 	// This is precipIntensity
 	if (cur.precipIntensity < 0.002){
@@ -93,10 +109,52 @@ function data_deploy(data, city, state, celsius){
 	$('#current-weather-table #Dew-Point').text(cur.dewPoint.toFixed(2));
 	$('#current-weather-table #Humidity').text(cur.humidity.toFixed(2) * 100 + '%');
 	$('#current-weather-table #Visibility').text(cur.visibility.toFixed(2));
-	$('#current-weather-table #Sunrise').text(data.daily.data[0].sunriseTime);
-	$('#current-weather-table #Sunset').text(data.daily.data[0].sunsetTime);
+	$('#current-weather-table #Sunrise').text(time_transfer(data.daily.data[0].sunriseTime));
+	$('#current-weather-table #Sunset').text(time_transfer(data.daily.data[0].sunsetTime));
+
+	if (celsius){
+		$('#current-weather-sum #current-weather-temp').append('<sup>℃</sup>');
+		$('#current-weather-table #Wind-Speed').append('m/s');
+		$('#current-weather-table #Dew-Point').append('℃');
+		$('#current-weather-table #Visibility').append('km');
+	} else {
+		$('#current-weather-sum #current-weather-temp').append('<sup>℉</sup>');
+		$('#current-weather-table #Wind-Speed').append('mph');
+		$('#current-weather-table #Dew-Point').append('℉');
+		$('#current-weather-table #Visibility').append('mi');
+	}
 
 	GetMap(data.latitude, data.longitude);
+
+	/* part 2: display 24hour weather */
+	$('.oneday-tr').remove();
+	var hour = data.hourly;
+
+	for (var i = 0; i < 24; i++){
+		var time_str = time_transfer(hour.data[i].time);
+		var icon_url = icon_transfer(hour.data[i].icon);
+		var cloud_cover = hour.data[i].cloudCover.toFixed(2) * 100 + '%';
+		var temp = hour.data[i].temperature;
+		var wind = hour.data[i].windSpeed;
+		var humidity = hour.data[i].humidity.toFixed(2) * 100 + '%';
+		var visibility = hour.data[i].visibility;
+		var pressure = hour.data[i].pressure;
+
+		if (celsius){
+			wind = wind + 'm/s';
+			visibility = visibility + 'km';
+			pressure = pressure + 'kPa';
+		} else {
+			wind = wind + 'mph';
+			visibility = visibility + 'mi';
+			pressure = pressure + 'mb';
+		}
+
+		var sub_table = "<table class='table'><thead><tr><th class='col-xs-3 col-sm-3'>Wind</th><th class='col-xs-3 col-sm-3'>Humidity</th><th class='col-xs-3 col-sm-3'>Visibility</th><th class='col-xs-3 col-sm-3'>Pressure</th></tr></thead><tr><td>"+wind+"</td><td>"+humidity+"</td><td>"+visibility+"</td><td>"+pressure+"</td></tr></table>";
+
+		$('#oneday-table').append("<tr class='oneday-tr'><td>"+time_str+"</td><td><img style='max-height:50px;' src='"+icon_url+"'></td><td>"+cloud_cover+"</td><td>"+temp+"</td><td><button><span class='glyphicon glyphicon-plus' aria-hidden='true'></span></button></td></tr>");
+			//<a role='button' data-toggle='collapse' href='#oneday-collapse-"+i+"' aria-expanded='false' aria-controls='oneday-collapse-"+i+"'><span class='glyphicon glyphicon-plus' aria-hidden='true'></span></a></td></tr><div class='collapse' id='oneday-collapse-"+i+"'><div class='well'>"+sub_table+"</div></div>");
+	}
 }
 
 
